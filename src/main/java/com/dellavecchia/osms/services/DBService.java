57bc4fc8a402ef5bf33c34b5642a52ea4a81ad7f
Gabriel Guimarães <gabrieldellavecchia@gmail.com>
2023-10-25 10:11:04 -0300

    added more base technicians

--- src/main/java/com/dellavecchia/osms/services/DBService.java
+++ src/main/java/com/dellavecchia/osms/services/DBService.java
@@ -28,13 +28,17 @@ public class DBService {
 
         Technician t1 = new Technician(null, "Gabriel Guimarães", "100.531.916-25", "+5535991481551");
         Technician t2 = new Technician(null, "Marcio Mascarenas", "412.205.966-63", "+5519986545454");
+        Technician t3 = new Technician(null, "Maria da Silva", "920.981.390-12", "+5599987654321");
+        Technician t4 = new Technician(null, "João Santos", "874.525.550-40", "+5599777755555");
+        Technician t5 = new Technician(null, "Carlos Oliveira", "826.977.390-50", "+5599555544444");
+        Technician t6 = new Technician(null, "Luiza Almeida", "946.143.940-78", "+5599444433333");
         Client c1 = new Client(null, "Ivonir Della Vecchia Marques", "161.684.882-00", "(35) 99120-3731");
         OS os1 = new OS(null, "test create os", Priority.HIGH, Status.ONGOING, t1, c1);
 
         t1.getList().add(os1);
         c1.getList().add(os1);
 
-        technicianRepository.saveAll(Arrays.asList(t1, t2));
+        technicianRepository.saveAll(Arrays.asList(t1, t2, t3, t4, t5, t6));
         clientRepository.saveAll(Arrays.asList(c1));
         osRepository.saveAll(Arrays.asList(os1));
     }
